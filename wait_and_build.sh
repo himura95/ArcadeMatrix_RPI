21ceeb1ff214d203138d455b64d7e11af80b3aa2
Red1L <red77290@gmail.com>
2026-07-15 19:07:36 +0200

    chore: Revert image size back to 14G since we now use gzip compression

--- wait_and_build.sh
+++ wait_and_build.sh
@@ -7,4 +7,4 @@ echo "Docker is up! Running cleanup..."
 docker run --privileged debian:bookworm bash -c 'apt-get update && apt-get install -y kpartx && for i in $(losetup -a | grep ArcadeMatrix_Build | awk -F: "{print \$1}"); do kpartx -d $i; losetup -d $i; done'
 rm -f /tmp/ArcadeMatrix_Build*.img /workspace/ArcadeMatrix_Release.img ArcadeMatrix_Release.img
 echo "Starting build..."
-./scripts/build_image.sh 58G
+./scripts/build_image.sh 14G
