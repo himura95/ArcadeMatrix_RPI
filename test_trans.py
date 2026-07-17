314bfb139b5f017e24f235159aa68e680b98eece
Red1L <red77290@gmail.com>
2026-07-17 13:52:02 +0200

    chore: Remove leftover testing and debugging scrap scripts

--- test_trans.py
+++
@@ -1,7 +0,0 @@
-from PIL import Image
-import struct
-
-trans = 0x07E0 # Green
-trans_bytes = struct.pack('<H', trans)
-trans_pixel = Image.frombytes('RGB', (1, 1), trans_bytes, 'raw', 'BGR;16').getpixel((0,0))
-print(trans_pixel)
